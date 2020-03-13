0049d90ffd53b03c39ec2667be78c13abb3f83ac
Michael Bradley <mjb.bda@gmail.com>
2020-03-13 18:52:43 -0400

    Commenting updated

--- js/app.js
+++ js/app.js
@@ -5,18 +5,21 @@ $(document).foundation();
 // variable to hold the path user takes, needs to be cleared everytime they go home/make any changes. 
 let pathTaken = [];
 
+//variable to allow the various switches to work
 let cancertype;
 
 // variable which will hold the data-number value of the clicked node to be later used by the submit button.
 let clickedNodeFlag;
-let clickedNodeDesc;
+
 
 // the trial class for obtaining needed information related to the trials and nodes
 class Trials {
     constructor(nodeClicked) {
-        // array which holds all the trials for lungs, then later we filter the information we need
+        // arrays which holds all the trials for each type of cancer
         this.lungTrials;
         this.breastTrials;
+        this.urinaryTrials;
+        this.skinTrials
         // array which holds all the nodes for lungs chart
         this.lungNodes;
         this.skinNodes;
@@ -26,6 +29,7 @@ class Trials {
         // getting the trials
         // using the nodeClicked value to fetch the correct node. 
         this.fetchNodes(nodeClicked, cancertype);
+        //adding which button was clicked to the pathTaken array
         pathTaken.push(nodeClicked);
         
     }
@@ -59,7 +63,7 @@ class Trials {
                 break;
         }
 
-        // array to hold the nodes to be followed by the clicked node
+        // array to hold what node numbers follow the current node
         const nodesToFollow = nodeClickedInfo[0].nodesToFollow;
         // variable to hold the nodes' names 
         const nodesClickedName = nodeClickedInfo[0].name;
@@ -68,6 +72,7 @@ class Trials {
 
         // the foreach loop to create multiple nodes according to the nodesToFollow;
         document.getElementById("nodesHere").innerHTML = "";
+        //run the matchNode function if there are no more nodes to follow on a clicked option
         if (nodesToFollow.length == 0) {
             this.matchNodes();
         } else {
@@ -79,52 +84,54 @@ class Trials {
             divCrumb.classList.add("crumbBorder");
             crumbs.append(divCrumb);
             
+            //loop that runs through each element of the array nodesToFollow
             nodesToFollow.forEach(element => {
                 let createdDiv = document.createElement("div");
                 document.getElementById("nodesHere").append(createdDiv);
 
-
+                //using the loop, create the next set of buttons that will appear on the screen
                 let createdNode = document.createElement("input");
                 createdNode.type = "button";
                 createdNode.classList.add("large-6");
-                // getting the value from a function created just to get name of the nodes while passing it the node we want the name of.
+                // using gettingNodeName, we pass that information to createdNode.value so that it has the correct name
                 createdNode.value = this.gettingNodeName(element);
                 
+                //creating the informational button for each option
                 let iButton = document.createElement("button");
                 iButton.innerHTML = "i";
 
-                // adding the modal data attribute
-                iButton.setAttribute("data-open", "exampleModal1");
-                // iButton.setAttribute("src", "../images/i.png");
+                // adding the modal data attribute - allows the modal to be opened
+                iButton.setAttribute("data-open", "infoModal");
+                //adding the class i-button to the newly made button so that they are styled correctly.
                 iButton.classList.add("i-button");
+                //giving the description of the option to the i-button for later use
                 iButton.setAttribute('data-desc', this.gettingNodeDesc(element));
 
 
-                // $('.i-button').reveal({
-                //     animation: 'fadeAndPop',
-                //     animationspeed: 300,
-                //     closeOnBackgroundClick: true,
-                //     dismissModalClass: 'close-reveal-modal'
-                // });
 
-                // setting the data-number of the created element by using the function solely for getting the node number of the specific node.
+
+                // sassing the correct node number to the newly created button for later use
                 createdNode.setAttribute('data-number', this.gettingNodeNumber(element));
-                // createdNode.setAttribute('data-desc', this.gettingNodeDesc(element));
                 
                 
 
+                //put the newly created button in the correct position
                 createdDiv.append(createdNode);
                 
+                //put the newly created i-buttons in the correct position
                 createdDiv.insertBefore(iButton, createdNode);
                 
+                //runs when an option is selected.
                 createdNode.addEventListener("click", function () {
+                    //submit button is functional
                     submit.disabled = false;
                     // giving flag the value of the data-number to be used by the submit button
-                    clickedNodeDesc = iButton.dataset.desc;
                     clickedNodeFlag = createdNode.dataset.number;
 
                 });
                 
+                //runs when the information modal gets opened
+                //inserts the name and description of the selected option based on which i button is clicked
                 iButton.addEventListener('click', function (){
                     let iTitle = document.querySelector('h1');
                     let iDesc = document.querySelector('#result');
@@ -136,8 +143,10 @@ class Trials {
     }
 
 
+    //function that gets and returns the name of the node
     gettingNodeName(node) {
         let nodeName;
+        //switch to determine cancer type
         switch (cancertype) {
             case 0:
                 nodeName = this.lungNodes.filter(clickedNode => clickedNode.node == node);
@@ -156,8 +165,10 @@ class Trials {
         return nodeName[0].name;
     }
 
+    //function that gets the number of the individual nodes
     gettingNodeNumber(node) {
         let nodeNumber;
+        //switch to determine the cancer type
         switch (cancertype) {
             case 0:
                 nodeNumber = this.lungNodes.filter(clickedNode => clickedNode.node == node);
@@ -175,8 +186,10 @@ class Trials {
 
         return nodeNumber[0].node;
     }
+    //function that gets the description of the indiviual nodes
     gettingNodeDesc(node){
         let nodeDesc;
+        //switch determines which cancer type
         switch(cancertype){
             case 0:
                 nodeDesc = this.lungNodes.filter(clickedNode => clickedNode.node == node);
@@ -196,31 +209,28 @@ class Trials {
 
     gettingNodeQuestion(node) {
         let nodeNumber = this.lungNodes.filter(clickedNode => clickedNode.node == node);
-        console.log(nodeNumber[0].questionOnThisNode);
-
         return nodeNumber[0].questionOnThisNode;
     }
 
     //checks the user choices against available trials and if they are open to more patients or not.
     matchNodes() {
-        //output is the variable that will be outputted to the screen for the user to read
+        //hide the submit button
+        //unhide the reset button
+        //create variables for final screen output
         submit.classList.add("hide");
         reset.classList.remove("hide");
         pathTaken = pathTaken.join("")
-        console.log(pathTaken)
         let joinedSuccessfulPath;
-        //none is used to create the final breadCrumb Trail
-        
         let output = document.createElement("div");
         let divCrumb = document.createElement("div");
         let theCrumbP = document.createElement("p");
         
         divCrumb.append(theCrumbP);
-        let finalCrumb = document.createElement("div");
         divCrumb.classList.add("crumbBorder");
         //switch to keep track of what type of cancer
         switch (cancertype) {
             case 0:
+                //for loop to check the entire trial array
                 for (let i = 0; i < this.lungTrials.length; i++) {
                     joinedSuccessfulPath = this.lungTrials[i].pathToSuccessfulTrial.join("");
                     if (pathTaken == joinedSuccessfulPath) {
@@ -353,10 +363,12 @@ class Trials {
 
 
 
+    //function to fetch the button information based on which cancer type
     fetchNodes(nodeClicked, cancertype) {
         fetch('data/nodes.json')
             .then(data => data.json())
             .then(data => {
+                //switch that checks which type of cancer it is
                 switch (cancertype) {
                     case 0:
                         this.lungNodes = data.lungNodes;
@@ -403,8 +415,9 @@ let contact = document.getElementById("contact");
 let print = document.getElementById("print");
 let printP = document.getElementById("printPage");
 printP.style.display = "none";
-console.log(printP.style.display);
 
+
+//print button activation
 printP.addEventListener("click", function (){
     window.print();
 })
@@ -448,7 +461,9 @@ urinary.addEventListener("click", function () {
 // another click listener for the creation of following nodes to come.
 submit.addEventListener("click", getNew);
 
+//function that starts loading the next set of buttons
 function getNew() {
+    //stops the submit button from being useable
     submit.disabled = true;
     theStart(clickedNodeFlag);
     
@@ -456,17 +471,16 @@ function getNew() {
 
 
 function theStart(nodeClicked) {
-
     // passing the clicked node's data-number value to the class
     let tri = new Trials(nodeClicked);
 }
 
+//restart button and click event listener
 let reset = document.getElementById("reset");
 reset.addEventListener("click", resetApp);
 
 
-
+//reloads the program when restart button is clicked
 function resetApp() {
-    console.log("test")
     document.location.reload();
 }
